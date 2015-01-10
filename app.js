4fac93ffa77b456f62fff4d9ff937830e68514a6
mavidser <mavidser@gmail.com>
2015-01-10 17:44:40 +0530

    Changes for heroku

--- app.js
+++ app.js
@@ -2,21 +2,18 @@ var express = require('express');
 var jade = require('jade');
 var path = require('path');
 var routes = require('./routes');
-var port = 8000;
-var host = "127.0.0.1";
-
 var app = express();
 
 app.set('view engine', 'jade')
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('port', process.env.PORT || 8000);
+app.set('port', (process.env.PORT || 8000));
 
 var http = require('http').Server(app);
 app.get("/",routes.index);
 app.get("/ama?",routes.submit);
 app.get("/ama/:id",routes.ama);
 
-http.listen(port,host,function(){
-console.log("Server listening on "+host+":"+port);
+http.listen(app.get('port'),function(){
+console.log("Server listening on localhost:"+app.get('port'));
 });
